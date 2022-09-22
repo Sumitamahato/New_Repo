b8efe09199722c4264625630bef144d41087d12c
Tiwarysagar <sagartiwary354@gmail.com>
2022-09-22 23:25:50 +0530

    updated

--- prime.js
+++ prime.js
@@ -1,13 +1,13 @@
 let num=13
 count=0;
-for(i=0;i<=num.length;i++){
+for(i=1;i<=num.length;i++){
 
-if(num%i=0){
+if(num%i==0){
 count+
 }
   
 }
-  if(count=2){
+  if(count==2){
 
 console.log("prime no")
 }
